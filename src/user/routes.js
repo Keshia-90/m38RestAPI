601514ea1ed692a21b3a6e53972b0aafc3d42431
Keshia Collins <keshia90.kc@googlemail.com>
2022-07-08 10:00:22 +0100

    double check everything has been pushed to repo

--- src/user/routes.js
+++ src/user/routes.js
@@ -6,7 +6,7 @@ const userRouter = Router(); //create a router that can have endpoints added to 
 userRouter.post("/user", hashPass, signUp); //defining a post request on /user path, that calls the signUp controller
 userRouter.post("/login", comparePass, login); //defining a post request on /login path, that calls the login controller
 userRouter.get("/token", tokenCheck, login);
-userRouter.get("/user/:username", listUser);//params
+userRouter.get("/user/:username", listUser);//params added
 userRouter.delete("/user/:username", deleteOne);
 userRouter.get("/user", findAll);
 
